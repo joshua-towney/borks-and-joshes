3fa59284de709090b3409cc753d8396fbf60059f
Joshua Towney <joshtowney@hotmail.com>
2021-12-02 12:50:26 +1100

    planning added

--- borks-and-joshes.js
+++ borks-and-joshes.js
@@ -1,1 +1,21 @@
 console.log('Wlcome to Borks and Joshes')
+
+var button = document.querySelectorAll('button');
+
+
+function buttonHoverColor(event) {
+    let theOneHoveredOn = event.target;
+    theOneHoveredOn.style.backgroundColor = "lightblue"
+}
+
+function buttonNoHoverColor(event) {
+    let theOneHoveredOn = event.target;
+    theOneHoveredOn.style.backgroundColor = "white"
+}
+
+button.forEach(function(scrollOver) {
+    scrollOver.addEventListener('mouseover', buttonHoverColor)
+});
+button.forEach(function(scrollOver) {
+    scrollOver.addEventListener('mouseout', buttonNoHoverColor)
+});
